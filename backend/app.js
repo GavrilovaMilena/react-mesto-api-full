bf195715faa0fa101d36d7a449592e56cefe1f80
GavrilovaMilena <mia.gggg@yandex.ru>
2021-05-21 18:33:06 +0300

    ПР15 исправления после первого ревью

--- backend/app.js
+++ backend/app.js
@@ -81,7 +81,9 @@ app.use((err, req, res, next) => {
   const { message } = err;
   const statusCode = err.statusCode || 500;
   res.status(statusCode).send({
-    message, // statusCode === 500 ? 'Произошла ошибка на сервере' : message,
+    message: statusCode === 500
+      ? 'Произошла ошибка на сервере'
+      : message,
   });
   next();
 });
